de7839bd883a5547b845764cdd949911b361db7d
EC2 Default User <ec2-user@ip-172-31-24-34.ec2.internal>
2021-02-01 12:37:23 +0000

    favorite for comment completed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :book
   has_many :favorites, dependent: :destroy
+  
+  def favorited_by?(user)
+    favorites.where(user_id: user).exists?
+  end
 end
